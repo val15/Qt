4ba395e176f1789954bc0b434a2a9c32b345b573
val15 <randrenarizoval@gmail.com>
2018-01-26 10:25:29 +0300

    apres correction du debit du serveur et unification du debop Qt

---
+++ arduinoSerieHorlogeIneractifServeur5is-iesi/build-fenetre0-Desktop-Debug/moc_Fenetre0.cpp
@@ -0,0 +1,240 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'Fenetre0.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../fenetre0/Fenetre0.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'Fenetre0.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.3.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MaFenetre_t {
+    QByteArrayData data[37];
+    char stringdata[642];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MaFenetre_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MaFenetre_t qt_meta_stringdata_MaFenetre = {
+    {
+QT_MOC_LITERAL(0, 0, 9),
+QT_MOC_LITERAL(1, 10, 31),
+QT_MOC_LITERAL(2, 42, 0),
+QT_MOC_LITERAL(3, 43, 4),
+QT_MOC_LITERAL(4, 48, 5),
+QT_MOC_LITERAL(5, 54, 35),
+QT_MOC_LITERAL(6, 90, 14),
+QT_MOC_LITERAL(7, 105, 12),
+QT_MOC_LITERAL(8, 118, 40),
+QT_MOC_LITERAL(9, 159, 26),
+QT_MOC_LITERAL(10, 186, 4),
+QT_MOC_LITERAL(11, 191, 29),
+QT_MOC_LITERAL(12, 221, 36),
+QT_MOC_LITERAL(13, 258, 23),
+QT_MOC_LITERAL(14, 282, 16),
+QT_MOC_LITERAL(15, 299, 22),
+QT_MOC_LITERAL(16, 322, 5),
+QT_MOC_LITERAL(17, 328, 17),
+QT_MOC_LITERAL(18, 346, 10),
+QT_MOC_LITERAL(19, 357, 4),
+QT_MOC_LITERAL(20, 362, 15),
+QT_MOC_LITERAL(21, 378, 27),
+QT_MOC_LITERAL(22, 406, 27),
+QT_MOC_LITERAL(23, 434, 11),
+QT_MOC_LITERAL(24, 446, 7),
+QT_MOC_LITERAL(25, 454, 26),
+QT_MOC_LITERAL(26, 481, 20),
+QT_MOC_LITERAL(27, 502, 22),
+QT_MOC_LITERAL(28, 525, 14),
+QT_MOC_LITERAL(29, 540, 20),
+QT_MOC_LITERAL(30, 561, 3),
+QT_MOC_LITERAL(31, 565, 18),
+QT_MOC_LITERAL(32, 584, 11),
+QT_MOC_LITERAL(33, 596, 15),
+QT_MOC_LITERAL(34, 612, 3),
+QT_MOC_LITERAL(35, 616, 16),
+QT_MOC_LITERAL(36, 633, 8)
+    },
+    "MaFenetre\0lectureDesDatesImportantsDuMois\0"
+    "\0year\0month\0repLectureDesDatesImportantesDuMois\0"
+    "indiceEnvoyeur\0typeDuSeveur\0"
+    "repLectureDesDatesImportantesDuMoisATout\0"
+    "lectureDesEvenementsDuJour\0date\0"
+    "repLectureDesEvenementsDuJour\0"
+    "repLectureDesEvenementsDuJourAndroid\0"
+    "repLectureDesEvenements\0ajouterEvenement\0"
+    "selectionTypeEvenement\0texte\0"
+    "ajouterEnregister\0connection\0temp\0"
+    "gererCalendrier\0lireEvenementDuJourEtProch1\0"
+    "actiVOuDesactiVClignotement\0setRecepton\0"
+    "txtRecu\0desativerActiverAllumeAuto\0"
+    "actiVOueteindrePrise\0allumerOuEteindreLumiR\0"
+    "eteindreAlarme\0traiterMessagServeur\0"
+    "msg\0deconnectionClient\0ecrirConfig\0"
+    "exeEcrireConfig\0rqt\0repLectureConfig\0"
+    "miseMA38"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MaFenetre[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      28,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    2,  154,    2, 0x08 /* Private */,
+       5,    4,  159,    2, 0x08 /* Private */,
+       8,    2,  168,    2, 0x08 /* Private */,
+       1,    0,  173,    2, 0x08 /* Private */,
+       9,    1,  174,    2, 0x08 /* Private */,
+      11,    3,  177,    2, 0x08 /* Private */,
+      12,    3,  184,    2, 0x08 /* Private */,
+      13,    2,  191,    2, 0x08 /* Private */,
+       9,    0,  196,    2, 0x08 /* Private */,
+      14,    0,  197,    2, 0x08 /* Private */,
+      15,    1,  198,    2, 0x08 /* Private */,
+      17,    0,  201,    2, 0x08 /* Private */,
+      18,    0,  202,    2, 0x08 /* Private */,
+      19,    0,  203,    2, 0x08 /* Private */,
+      20,    0,  204,    2, 0x08 /* Private */,
+      21,    0,  205,    2, 0x08 /* Private */,
+      22,    0,  206,    2, 0x08 /* Private */,
+      23,    1,  207,    2, 0x08 /* Private */,
+      25,    0,  210,    2, 0x08 /* Private */,
+      26,    0,  211,    2, 0x08 /* Private */,
+      27,    0,  212,    2, 0x08 /* Private */,
+      28,    0,  213,    2, 0x08 /* Private */,
+      29,    3,  214,    2, 0x08 /* Private */,
+      31,    0,  221,    2, 0x08 /* Private */,
+      32,    0,  222,    2, 0x08 /* Private */,
+      33,    1,  223,    2, 0x08 /* Private */,
+      35,    0,  226,    2, 0x08 /* Private */,
+      36,    0,  227,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    3,    4,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Int,    3,    4,    6,    7,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    3,    4,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QDate,   10,
+    QMetaType::Void, QMetaType::QString, QMetaType::Int, QMetaType::Int,   10,    6,    7,
+    QMetaType::Void, QMetaType::QString, QMetaType::Int, QMetaType::Int,   10,    6,    7,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    6,    7,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   16,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   24,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, QMetaType::Int, QMetaType::Int,   30,    6,    7,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QStringList,   34,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MaFenetre::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MaFenetre *_t = static_cast<MaFenetre *>(_o);
+        switch (_id) {
+        case 0: _t->lectureDesDatesImportantsDuMois((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 1: _t->repLectureDesDatesImportantesDuMois((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3])),(*reinterpret_cast< int(*)>(_a[4]))); break;
+        case 2: _t->repLectureDesDatesImportantesDuMoisATout((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 3: _t->lectureDesDatesImportantsDuMois(); break;
+        case 4: _t->lectureDesEvenementsDuJour((*reinterpret_cast< QDate(*)>(_a[1]))); break;
+        case 5: _t->repLectureDesEvenementsDuJour((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3]))); break;
+        case 6: _t->repLectureDesEvenementsDuJourAndroid((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3]))); break;
+        case 7: _t->repLectureDesEvenements((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 8: _t->lectureDesEvenementsDuJour(); break;
+        case 9: _t->ajouterEvenement(); break;
+        case 10: _t->selectionTypeEvenement((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 11: _t->ajouterEnregister(); break;
+        case 12: _t->connection(); break;
+        case 13: _t->temp(); break;
+        case 14: _t->gererCalendrier(); break;
+        case 15: _t->lireEvenementDuJourEtProch1(); break;
+        case 16: _t->actiVOuDesactiVClignotement(); break;
+        case 17: _t->setRecepton((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 18: _t->desativerActiverAllumeAuto(); break;
+        case 19: _t->actiVOueteindrePrise(); break;
+        case 20: _t->allumerOuEteindreLumiR(); break;
+        case 21: _t->eteindreAlarme(); break;
+        case 22: _t->traiterMessagServeur((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3]))); break;
+        case 23: _t->deconnectionClient(); break;
+        case 24: _t->ecrirConfig(); break;
+        case 25: _t->exeEcrireConfig((*reinterpret_cast< QStringList(*)>(_a[1]))); break;
+        case 26: _t->repLectureConfig(); break;
+        case 27: _t->miseMA38(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MaFenetre::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_MaFenetre.data,
+      qt_meta_data_MaFenetre,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *MaFenetre::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MaFenetre::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_MaFenetre.stringdata))
+        return static_cast<void*>(const_cast< MaFenetre*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int MaFenetre::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 28)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 28;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 28)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 28;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
